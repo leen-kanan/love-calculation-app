603632ce9e59d2dc6510a9e3dcb86e2c0a74dace
Leen Kanan <leenkanan01@gmail.com>
2022-09-16 18:45:11 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  function love() {
+    const name1 = document.querySelector(".firstName").value;
+    const name2 = document.querySelector(".secondName").value;
+    if (name1.length <= 2) {
+      alert("Enter at least 3 character");
+    } else if (name1 === "leen" && name2 === "moe") {
+      document.querySelector(".result").innerHTML =
+        name1 + " and " + name2 + " = " + 100 + "%" + " of love ❤️";
+    } else {
+      let random = Math.floor(Math.random() * 100);
+      document.querySelector(".result").innerHTML =
+        name1 + " " + "and " + name2 + " = " + random + "%" + " of love";
+    }
+    const form = document.querySelector(".inputs");
+    function handleForm(event) {
+      event.preventDefault();
+    }
+    form.addEventListener("submit", handleForm);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>❥Love Calculator</h2>
+      <form className="inputs">
+        <input
+          type="text"
+          placeholder="Your Name"
+          className="firstName"></input>
+        <input
+          type="text"
+          placeholder="Your Lover"
+          className="secondName"></input>
+        <button onClick={love}>Calculate</button>
+      </form>
+      <p className="result">Your relationship result here</p>
     </div>
   );
 }
