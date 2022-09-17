7db7fd9d056f9c04734ce63222d2b7d151bbcb2f
Leen Kanan <leenkanan01@gmail.com>
2022-09-16 19:44:12 -0500

    simple changes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  function love() {
+  const [results, setResults] = useState("Your relationship result here");
+
+  const love = () => {
     const name1 = document.querySelector(".firstName").value;
     const name2 = document.querySelector(".secondName").value;
-    if (name1.length <= 2) {
-      alert("Enter at least 3 character");
-    } else if (name1 === "leen" && name2 === "moe") {
-      document.querySelector(".result").innerHTML =
-        name1 + " and " + name2 + " = " + 100 + "%" + " of love ❤️";
+    if (name1.length <= 1 || name2.length <= 1) {
+      alert("Enter at least 2 character");
+    } else if (
+      (name1 === "leen" && name2 === "moe") ||
+      (name1 === "moe" && name2 === "leen")
+    ) {
+      setResults(name1 + " and " + name2 + " = " + 100 + "%" + " of love ❤️");
     } else {
-      let random = Math.floor(Math.random() * 100);
-      document.querySelector(".result").innerHTML =
-        name1 + " " + "and " + name2 + " = " + random + "%" + " of love";
+      const nameLength1 = name1.length;
+      const nameLength2 = name2.length;
+
+      const results = Math.floor(
+        (Math.min(nameLength1, nameLength2) /
+          Math.max(nameLength1, nameLength2)) *
+          100
+      );
+
+      setResults(name1 + " and " + name2 + " = " + results + "%");
     }
-    const form = document.querySelector(".inputs");
-    function handleForm(event) {
-      event.preventDefault();
-    }
-    form.addEventListener("submit", handleForm);
-  }
+  };
 
   return (
     <div className="container">
       <h2>❥Love Calculator</h2>
-      <form className="inputs">
+      <div className="inputs">
         <input
           type="text"
           placeholder="Your Name"
@@ -35,8 +41,8 @@ function App() {
           placeholder="Your Lover"
           className="secondName"></input>
         <button onClick={love}>Calculate</button>
-      </form>
-      <p className="result">Your relationship result here</p>
+      </div>
+      <p className="result">{results}</p>
     </div>
   );
 }
